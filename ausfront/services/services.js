9936adec410d69b60422a10d497681aad891dbd1
bearom95 <bearom95@gmail.com>
2022-10-13 01:34:18 +0200

    render y cambio de direccion fetch

--- ausfront/services/services.js
+++ ausfront/services/services.js
@@ -1,7 +1,7 @@
 
 export const getData = async (param) => {
     try {
-      const data = await fetch(`http://localhost:8080/${param}`);
+      const data = await fetch(`https://aus-project-back.onrender.com/${param}`);
       const dataJSON = await data.json();
       return dataJSON;
     } catch (error) {
